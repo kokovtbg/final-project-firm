108af25f4b8b4d09529a095ca8de5bf6ef55326d
kokovtbg <kokovtbg@gmail.com>
2023-12-23 14:48:17 +0200

    final sql file

--- src/main/java/bg/sirma/project/sqlTemp/joinTable.sql
+++ src/main/java/bg/sirma/project/sqlTemp/joinTable.sql
@@ -1,5 +1,5 @@
 SELECT
-max(
+sum(
 CASE
 WHEN
 (empl1.start_date <= empl2.start_date
@@ -28,13 +28,17 @@ AND empl1.start_date <= ifnull(empl1.end_date, curdate())
 AND ifnull(empl1.end_date, curdate()) <= ifnull(empl2.end_date, curdate()))
 THEN
 datediff(ifnull(empl1.end_date, curdate()), empl1.start_date)
-END) AS diff,
+END
+) AS diff,
 
 empl1.employee_id AS first_employee,
 empl2.employee_id AS second_employee,
-empl1.project_id
+group_concat(DISTINCT empl1.project_id)
 
 FROM s_employees.employee_projects AS empl1
 JOIN s_employees.employee_projects AS empl2
 ON empl1.project_id = empl2.project_id
-AND empl1.employee_id != empl2.employee_id
+AND empl1.employee_id < empl2.employee_id
+GROUP BY empl1.employee_id, empl2.employee_id
+ORDER BY `diff` DESC
+LIMIT 1
